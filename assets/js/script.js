74cbbe1282fcbb73cf71b1e8384e8f3c11d1d500
GueryMathieu <hamta15300@gmail.com>
2019-05-28 17:00:53 +0200

    wsfinal

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function pourcentage(pourcentage, var2,){
+    return var2 * (pourcentage/100)
+}
+  
+  
+  //sticky navbar//////////////////////////////////////////////////////////////
+  
+  window.onscroll = function() {myFunction()};
+  
+  var navbar = document.getElementById("nav");
+  var sticky = navbar.offsetTop;
+  var windowWidth = window.innerWidth
+  
+  
+  function myFunction() {
+    if(windowWidth > 1140){
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky", "is-sticky")
+      } else {
+        navbar.classList.remove("sticky","is-sticky");
+      }
+    } else {
+        navbar.classList.add("sticky", "is-sticky") 
+    }
+  }
